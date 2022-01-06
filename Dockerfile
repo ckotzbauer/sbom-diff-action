80864e724de39dc18bcafdddbe046cd0317d4647
Christian Kotzbauer <git@ckotzbauer.de>
2022-01-06 17:34:02 +0100

    initial commit
    Signed-off-by: Christian Kotzbauer <git@ckotzbauer.de>

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM alpine:3.15
+
+ENV SYFT_VERSION 0.34.0
+
+RUN apk --no-cache add curl jq && \
+    wget -O syft.tar.gz https://github.com/anchore/syft/releases/download/v${SYFT_VERSION}/syft_${SYFT_VERSION}_linux_amd64.tar.gz && \
+    syft_sha256='0efee88a90e46f86be62f45cdeb416d2e0734dcfebc6f4fec6b41bcbbd2645fa' && \
+    echo "$syft_sha256  syft.tar.gz" | sha256sum -c - && \
+    mkdir -p /usr/share/syft && \
+    tar -C /usr/share/syft -oxzf syft.tar.gz && \
+    ln -s /usr/share/syft/syft /usr/bin/syft && \
+    rm syft.tar.gz
+
+COPY ./sbom_diff.sh /
+CMD /sbom_diff.sh
---
+++ sbom_diff.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -x # verbose mode
+set -e # stop executing after error
+
+echo "Starting Syft diff script"
+
+
+mkdir /syft-diff-action
+mkdir /syft-diff-action/new
+mkdir /syft-diff-action/old
+mkdir /syft-diff-action/workspace
+cp --archive /github/workspace/. /syft-diff-action/workspace
+chmod -R a+w /syft-diff-action
+cd /syft-diff-action/workspace
+
+
+if [ ! "$GITHUB_EVENT_NAME" = "pull_request" ]; then
+    echo "Not a PR!"
+    exit 0
+fi
+
+api_url="$(cat $GITHUB_EVENT_PATH | jq --raw-output '.pull_request.comments_url')"
+ancestor_sha="$(git merge-base $GITHUB_SHA origin/$GITHUB_BASE_REF)" # find the closest common ancestor between the PR branch and the target branch
+
+syft ${INPUT_SCAN_TARGET} -o table >../new/sbom.txt
+git checkout --force $ancestor_sha
+syft ${INPUT_SCAN_TARGET} -o table >../old/sbom.txt
+diff="$(diff -u0 ../old/sbom.txt ../new/sbom.txt || true)" # 'or true' because a non-identical diff outputs 1 as the exit status
+
+
+comment="\`\`\`diff
+$diff
+\`\`\`"
+
+curl --include --verbose --fail \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-H "Authorization: token ${GITHUB_TOKEN}" \
+--request POST --data "$(jq --null-input --arg escaped_diff "$comment" '{body: $escaped_diff}')" \
+$api_url
+
+
+echo "Done"
