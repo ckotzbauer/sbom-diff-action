08d8df1be4c370cc45126816b812ba7bf188b80e
Christian Kotzbauer <git@ckotzbauer.de>
2022-01-06 18:39:15 +0100

    fix: final changes
    Signed-off-by: Christian Kotzbauer <git@ckotzbauer.de>

--- sbom_diff.sh
+++ sbom_diff.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-set -x # verbose mode
+#set -x # verbose mode
 set -e # stop executing after error
 
 echo "Starting Syft diff script"
 
 
 mkdir /syft-diff-action
-mkdir /syft-diff-action/new
-mkdir /syft-diff-action/old
 mkdir /syft-diff-action/workspace
 cp --archive /github/workspace/. /syft-diff-action/workspace
 chmod -R a+w /syft-diff-action
@@ -23,14 +21,12 @@ fi
 api_url="$(cat $GITHUB_EVENT_PATH | jq --raw-output '.pull_request.comments_url')"
 ancestor_sha="$(git merge-base $GITHUB_SHA origin/$GITHUB_BASE_REF)" # find the closest common ancestor between the PR branch and the target branch
 
-syft ${INPUT_SCAN_TARGET} -o table >../new/sbom.txt
+syft ${INPUT_SCAN_TARGET} -o table >../pr-sbom.txt
 git checkout --force $ancestor_sha
-syft ${INPUT_SCAN_TARGET} -o table >../old/sbom.txt
-diff=$(diff -U 0 ../old/sbom.txt ../new/sbom.txt || true)
-#diff=$(echo $diff | tail +3)
-echo $diff
+syft ${INPUT_SCAN_TARGET} -o table >../base-sbom.txt
+diff=$(diff -U 0 ../base-sbom.txt ../pr-sbom.txt || true)
 
-message="SBOM-Diff for target \`${INPUT_SCAN_TARGET}\`:"
+message="SBOM-Diff for target \`${INPUT_SCAN_TARGET}\`"
 message="$message"$'\n'$'\n'
 if [ -z "$diff" ]; then
 message="$message"'```'$'\n'"No changes in detected."$'\n''```'
